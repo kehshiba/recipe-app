0957f679bae6d497921ab46da7aa5eeb04127ac5
Abhishek A.B. <57587313+kehshiba@users.noreply.github.com>
2024-12-01 23:41:40 +0530

    add

---
+++ app/src/main/java/com/abhishek/vegtummy/RoomDB/UserDao.java
@@ -0,0 +1,15 @@
+package com.abhishek.vegtummy.RoomDB;
+
+import androidx.room.Dao;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface UserDao {
+
+    @Query("SELECT * FROM recipe")
+    List<User> getAll();
+
+}
+
